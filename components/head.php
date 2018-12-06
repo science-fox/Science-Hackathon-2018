caea5f6668bf5e84f646b2fbf4552d25ea136de3
guy.sartorelli@gmail.com <guy.sartorelli@gmail.com>
2018-12-07 11:01:11 +1300

    fixed styles back from old proj

--- components/head.php
+++ components/head.php
@@ -2,6 +2,6 @@
   <meta charset="UTF-8" />
   <title>Website - <?=$title?></title>
   <link rel="stylesheet" type='text/css' href="/hackathon/style.css" />
-  <link rel="stylesheet" type='text/css' href="/bootstrap/bootstrap.min.css">
-  <link rel="shortcut icon" type="image/ico" href="/images/favicon.ico">
+  <link rel="stylesheet" type='text/css' href="/hackathon/bootstrap/bootstrap.min.css">
+  <link rel="shortcut icon" type="image/ico" href="/hackathon/images/favicon.ico">
 </head>
